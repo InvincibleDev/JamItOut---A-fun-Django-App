3ca9c0f51de38ad6f8ab061bef5257f5eac4f9ef
root <root@ip-172-31-37-134.us-east-2.compute.internal>
2019-03-24 15:49:13 +0000

    from server

--- jamitout/settings.py
+++ jamitout/settings.py
@@ -23,11 +23,12 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'vcw1dbm1en*h!lq914+gwt1zv)!#k$_s2d9mvt$i9(xq7p+e%='
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["jamitout.ga" ]
 
-
+CSRF_COOKIE_SECURE = True
+X_FRAME_OPTIONS='DENY'
 # Application definition
 
 INSTALLED_APPS = [
@@ -44,6 +45,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -138,9 +140,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
-STATICFILES_DIRS=[
-    os.path.join(BASE_DIR,"static")
-]
+STATIC_ROOT= os.path.join(BASE_DIR,'static')
 
 MEDIA_URL= '/media/'
 
